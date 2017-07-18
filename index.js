fdee332657ad1caef1b741fdd8f631b64d79f71d
bohorqux <bohorqux@bu.edu>
2017-07-18 17:58:06 +0000

    HALF AUTO SCROLL: FUNCTIONALITY COMPLETE
    - Webpage now scrolls automatically to specified section (manipulates the scrollbar
      within the div)

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-$("#goToProjects li").click(function() {
-    $('html, body').animate({
-	scrollTop: $("#_projects").offset().top}, 'slow');
+$(document).ready(function(){
+    var scrollSpeed = 1200;
+    var posNext = 400;
+/*
+    var $leftItems = $(".about-left li");
+    $leftItems.click(function(){
+	$leftItems.removeClass('act');
+	$(this).addClass('act');
+    });
+*/
+    $("#goToInternProgram").click(function(){
+	$(".about-right").animate({
+	    scrollTop: 0}, scrollSpeed);
+    });
+
+    $("#goToMentoring").click(function(){
+	$('.about-right').animate({
+	    scrollTop: posNext}, scrollSpeed);
+    });
+
+    $("#goToRoundtables").click(function(){
+	$('.about-right').animate({
+	    scrollTop: posNext*2}, scrollSpeed);
+   });
+
+    $("#goToProjects").click(function(){
+	$('.about-right').animate({
+	    scrollTop: posNext*3}, scrollSpeed);
+    });
+
+    $("#goToCollaterol").click(function(){
+	$('.about-right').animate({
+	    scrollTop:posNext*4}, scrollSpeed);
+    });
+
 });
+
+
