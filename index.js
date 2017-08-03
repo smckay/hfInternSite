701916f87e3bcba15d021234085325883925af5c
bohorqux <bohorqux@bu.edu>
2017-08-03 13:10:06 +0000

    AUTOSCROLLING AND RESPONSIVENESS UPDATE
    - autoscrolling is now responsive, buttons no longer overshoot target when scrolling is activated on different screens.
    - navbar is now activated, scrolling to specified sections, also responsive.
    - added padding classes between sections in order to avoid overlapping text on mobile device. classes are activated when document detects mobile device.

--- index.js
+++ index.js
@@ -15,25 +15,43 @@ function smoothScrollTo(itemClick, itemScrollActiv, posValue, scrollSpeed) {
 $(document).ready(function(){
     var scrollSpeed = 1200; //constants
     var posNext = 580;
-    var secNext = 230;
+    var secNext = 50;
 // SECTION VARIABLES: Insert the class name of the auto scrolling sections in their respective variables.
 // --------------------------------------------------------------------------
     var aboutSection = ".section-about-right";
     var internSection = ".section-intern-right";
+    var aboutHeight = $(window).height();
 // -------------------------------------------------------------------------- 
 
+    rH(".section-about-left");
+    rH(".section-about-right");
+    rH(".section-about-middle");
+    rH(".section-intern-left");
+    rH(".section-intern-right");
+    rH(".section-intern-middle");
+    rH(".section-HF-left");
+    rH(".section-HF-right");
+    rH(".section-HF-middle");
+    rH(".section-left");
+    rH(".section-right");
+    rH(".section-middle");
+    
     smoothScrollTo("#scrollToAboutIntern", "html, body", $("#About").offset().top+secNext, scrollSpeed);
+
+    smoothScrollTo("#nav_scrollToAboutIntern", "html, body", $("#About").offset().top+secNext, scrollSpeed);
     
 // SECTION: ABOUT
 // All jQuery/Javascript functions pertaining to ABOUT section
 // --------------------------------------------------------------------------
     smoothScrollTo("#goToInternProgram", aboutSection, 0, scrollSpeed);
-    smoothScrollTo("#goToMentoring", aboutSection, posNext, scrollSpeed);
-    smoothScrollTo("#goToRoundtables", aboutSection, (posNext*2)-55, scrollSpeed);
-    smoothScrollTo("#goToProjects", aboutSection, (posNext*3)-120, scrollSpeed);
-    smoothScrollTo("#goToCollaterol", aboutSection, posNext*4, scrollSpeed);
+    smoothScrollTo("#goToMentoring", aboutSection, aboutHeight-100, scrollSpeed);
+    smoothScrollTo("#goToRoundtables", aboutSection, aboutHeight-200, scrollSpeed);
+    smoothScrollTo("#goToProjects", aboutSection, aboutHeight, scrollSpeed);
+    smoothScrollTo("#goToCollaterol", aboutSection, aboutHeight, scrollSpeed);
     
-    smoothScrollTo("#scrollToExperience", "html, body", $("#Experience").offset().top+secNext, scrollSpeed);
+    smoothScrollTo("#scrollToExperience", "html, body", $("#Experience").offset().top + secNext, scrollSpeed);
+
+    smoothScrollTo("#nav_scrollToExperience", "html, body", $("#Experience").offset().top + secNext, scrollSpeed);
 // --------------------------------------------------------------------------
 
 // SECTION: INTERN
@@ -42,6 +60,13 @@ $(document).ready(function(){
     smoothScrollTo("#goToInternExperience", internSection, 0, scrollSpeed);
     smoothScrollTo("#goToVideos", internSection, posNext, scrollSpeed);
     smoothScrollTo("#goToTestimonials", internSection, posNext*2, scrollSpeed);
+
+
+    smoothScrollTo("#nav_scrollToAboutHF", "html, body", $("#AboutHF").offset().top+secNext, scrollSpeed);
+
+    smoothScrollTo("#nav_scrollToFAQ", "html, body", $("#FAQ").offset().top+secNext, scrollSpeed);
+
+    smoothScrollTo("#nav_scrollToConnect", "html, body", $("#Connect").offset().top+secNext, scrollSpeed);
 
     smoothScrollTo("#scrollToAboutHF", "html, body", $("#AboutHF").offset().top+secNext, scrollSpeed);
 
@@ -53,6 +78,12 @@ $(document).ready(function(){
 // --------------------------------------------------------------------------
 });
 
+function rH(divtag) {
+    $(window).resize(function() {
+	var newHeight = $(window).height() * 0.505;
+	$(divtag).height(newHeight);
+    }).resize();
+}
 
 function mouseOverOne(){
 	document.getElementById("answer1").style.display = "block";
